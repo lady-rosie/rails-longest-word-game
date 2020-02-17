fea60eab526c559be2f73174c5eca74f2fef4b98
Rose Grondin <rose.grondin@gmail.com>
2020-02-17 16:16:41 -0500

    create a longest word game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,15 @@
 class GamesController < ApplicationController
+  require 'open-uri'
+  require 'json'
+
   def new
     @letters = []
-    9.times { @letters << ('A'..'Z').to_a.sample}
+    9.times { @letters << ('A'..'Z').to_a.sample }
   end
 
   def score
-
+    @word = params[:word]
+    @dictionnary = JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{@word}").read)
+    @grid = params[:grid]
   end
 end

e2308a5854f832067e22d5197badaa529f6592b1
Rose Grondin <rose.grondin@gmail.com>
2020-02-17 16:38:20 -0500

    add the score to the game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,12 +4,15 @@ class GamesController < ApplicationController
 
   def new
     @letters = []
-    9.times { @letters << ('A'..'Z').to_a.sample }
+    8.times { @letters << ('A'..'Z').to_a.sample }
+    @letters << ['A', 'E', 'I', 'O', 'U', 'Y'].sample
   end
 
   def score
+    @end_time = Time.now
     @word = params[:word]
     @dictionnary = JSON.parse(open("https://wagon-dictionary.herokuapp.com/#{@word}").read)
     @grid = params[:grid]
+    @score = params[:word].length * 10
   end
 end
